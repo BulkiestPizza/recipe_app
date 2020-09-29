f4988968613a7d1ee2f63dad4861ddeff2178839
Lachlan Knell <49586571+BulkiestPizza@users.noreply.github.com>
2020-09-29 14:26:48 +1000

    Update main.dart
    12PM 29/09/20

--- lib/main.dart
+++ lib/main.dart
@@ -7,23 +7,73 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final title = 'Long List';
+    return Recipes();
+  }
+}
 
+class Recipes extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Recipes';
     return MaterialApp(
       title: title,
       home: Scaffold(
         appBar: AppBar(
           title: Text(title)
         ),
-      body: ListView.builder(
-        itemCount: 1000,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('100')
-          );
-        }
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('Chicken Soup'),
+            onTap: () {
+              
+            }
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+         Navigator.push(
+           context,
+           MaterialPageRoute(builder: (context) => New()),
+         );
+        },
+        child: Icon(Icons.add),
       ),
       ),
     );
   }
 }
+
+class Ingredients extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ingredients')
+      ),
+      body: ListView(
+      ),
+    );
+  }
+}
+
+class New extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: (
+        RaisedButton (
+          onPressed: () {
+            Navigator.pop(context);
+            Icon(Icons.arrow_back);
+          },
+        )
+        ),
+        title: Text('New Recipe'),
+      ),
+    );
+  }
+}
+
